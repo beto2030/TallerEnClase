41a06f7058b63e41c1a373f3b40975f6fbe485bf
angel <bucanero@misena.edu.co>
2020-06-27 21:17:04 -0500

    Primer commit para el ejercicio propueso en clase

---
+++ tipodemotordebomba.js
@@ -0,0 +1,35 @@
+
+
+
+
+    function resultado = Tipodemotor(){
+
+    var tipodemotor = prompt('Escriba tipo de bomba en numeros')
+
+    switch (tipodemotor){
+
+          case 1 :
+              alert( La bomba es una bomba de agua )
+                break;
+
+              case 2 :
+            alert( La bomba es una bomba de gasolina )
+
+              break;
+
+              casce 3 :
+              
+              alert( La bomba es una bomba de hormigón )
+                break;
+
+              case 4 :
+              alert( La bomba es una bomba de pasta alimenticia )
+
+                break;
+
+
+        
+    }
+
+
+Tipodemotor()
